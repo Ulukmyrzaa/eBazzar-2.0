136985a98e3b5eaf0cadf5cdcbc0ba5e34770545
Ulukmyrzaa <ulukbek.tsh@gmail.com>
2023-12-19 17:53:34 +0600

    Add file

--- carts/models.py
+++ carts/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.utils import timezone
-from accounts.models import CustomUser
+from accounts.models import User
 from myapp.models import Product
 
 
 class Basket(models.Model):
     id = models.AutoField(primary_key=True)
     final_price = models.DecimalField(max_digits=10, decimal_places=2)
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     creation_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

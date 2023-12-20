7566a4f74b2131ba94ca7fd35c1feb6480169224
sinqx <sinqxpls@gmail.com>
2023-12-20 13:17:21 +0600

    changes and fixes

--- carts/models.py
+++ carts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 from django.utils import timezone
 from accounts.models import User
-from myapp.models import Product
+from products.models import Product
 
 
 class Basket(models.Model):
@@ -9,7 +11,7 @@ class Basket(models.Model):
     final_price = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     creation_time = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return f'Basket #{self.id} - {self.user.username}'
 
@@ -29,18 +31,25 @@ class BasketItem(models.Model):
 
 
 class Order(models.Model):
-    STATUS_CHOICES = (
-        ('ожидает', 'Ожидает подтверждения'),
-        ('завершен', 'Завершен'),
-        ('отменен', 'Отменен'),
-    )
+    STATUS_CHOICES = [
+        ('WAITING_PAYMENT', 'Waiting Payment'),
+        ('PROCESSING', 'Processing'),
+        ('SHIPPED', 'Success'),
+        ('PAID', 'Paid'),
+        ('ERROR', 'Error'),
+        ('CANCELED', 'Canceled'),
+        ('REFUNDED', 'Refunded'),
+        ('PARTIALLY_SHIPPED', 'Partially shipped'),
+        ('ON_HOLD', 'On hold'),
+        ('SHIPPED', 'Shipped')
+    ]
 
     id = models.AutoField(primary_key=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.PositiveIntegerField()
     basket_item = models.OneToOneField(BasketItem, on_delete=models.CASCADE)
     product = models.OneToOneField(Product, on_delete=models.CASCADE)
-    buy_date = models.DateTimeField(default=timezone.now)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='ожидает')
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Waiting Payment')
     is_selected = models.BooleanField(default=False)
 
     def __str__(self):

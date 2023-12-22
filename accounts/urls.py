e77b24177e296ecf0c084d63d8b43e51514e4f86
akichun97 <akichun97@gmail.com>
2023-12-22 17:29:45 +0600

    urls for edit, delete, profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
+from flask_login import login_user
 from accounts.views import *
 
 urlpatterns = [
     path('', index, name='index'),
     path('signup/', Account.as_view(), name='signup'),
-    path('signin/', user_login, name='signin'),
-    path('signout/', user_logout, name='signout'),
+    path('signin/', LoginView.as_view(), name='signin'),
+    path('signout/', LogoutView.as_view(), name='signout'),
+    path('edit_profile/', EditView.as_view(), name='edit_profile'),
+    path('delete_user/', DeleteView.as_view(), name='delete_user'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+
 
 ]
 

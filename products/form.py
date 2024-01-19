13c62e32c78ebc36a0fe533402c12ea7cb703bc1
sinqx <sinqxpls@gmail.com>
2024-01-19 11:37:06 +0600

    final product forms update

--- products/form.py
+++ products/form.py
@@ -1,38 +1,40 @@
 from django import forms
 from .models import *
 from django import forms
-from django.db import transaction
 from .models import Product, ProductDetails
 
 
 class ProductDetailsForm(forms.ModelForm):
     class Meta:
         model = ProductDetails
-        fields = ["description", "prod_date", "exp_date"]
-
+        fields = [
+            "description",
+            "prod_date",
+            "exp_date",
+            "total_available",
+            "measurement_unit",
+        ]
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'price', "product_category"]
-
+        fields = ["name", "price", "product_category"]
 
 class CombinedProductForm(forms.ModelForm):
     product_form = ProductForm(prefix="product")
-    product_details_form = ProductDetailsForm(prefix="details")
 
     class Meta:
         model = Product
-        fields = ['name', 'price',  "product_category"]
+        fields = ["name", "price", "product_category"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields.update(self.product_details_form.fields)
+        self.fields.update(ProductDetailsForm(prefix="details").fields)
 
-    
+
 class SellerProductDetailsForm(forms.ModelForm):
     class Meta:
-        model = SellerProductDetails
+        model = SellerProductInfo
         fields = "__all__"
 
 

132c7effc41887714f4706a0ac7b40fe4778045b
sinqx <sinqxpls@gmail.com>
2024-05-22 17:13:47 +0600

    add all product path

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from products import *
 urlpatterns = [
     path("create/", CreateProductView.as_view(), name="create-product"),
     path('<slug:slug>/', ProductDetailsView.as_view(), name='product_detail'),
+    path('all/', ProductListView.as_view(), name='all_products'),
 ]

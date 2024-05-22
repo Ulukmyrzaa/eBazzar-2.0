455befef4758ab7e0f2525fda945a9a5778f3215
sinqx <sinqxpls@gmail.com>
2024-05-22 17:16:14 +0600

    update

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from accounts.models import UserDetails, WishList
+from accounts.models import User, WishList
 
 
-@receiver(post_save, sender=UserDetails)
-def create_wishlist(sender, instance, created, **kwargs):
-    if created:
-        WishList.objects.create(userDetails=instance)   
+# @receiver(post_save, sender=User)
+# def create_wishlist(sender, instance, created, **kwargs):
+#     if created:
+#         WishList.objects.create(user=instance)   

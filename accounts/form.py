679789eeb3e83d292641896284d0fcfecf2fda5a
sinqx <sinqxpls@gmail.com>
2024-05-22 17:15:50 +0600

    user variables names changes update

--- accounts/form.py
+++ accounts/form.py
@@ -1,19 +1,23 @@
 from django import forms
 from accounts.models import *
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+    UserChangeForm,
+)
 from products.models import Product
 
 
 class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
-        fields = ['appartmentAddress', 'street_address']
+        fields = ["apartment_number", "street_address"]
 
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['name', 'surname', 'phone_number', 'photo', 'gender']
+        fields = ["first_name", "last_name", "phone_number", "photo", "gender"]
 
     # Используйте AddressForm как форму для отображения и редактирования данных адреса
     address_form = AddressForm()
@@ -27,10 +31,10 @@ class ProfileForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super(ProfileForm, self).save(commit=False)
-        
+
         # Обработка данных формы адреса
         address_form = AddressForm(self.data, instance=self.instance.address)
-        
+
         if address_form.is_valid():
             address = address_form.save(commit=False)
             address.save()
@@ -39,55 +43,50 @@ class ProfileForm(forms.ModelForm):
         if commit:
             user.save()
 
-        return user 
-        
+        return user
+
 
 class RegisterForm(UserCreationForm):
-    appartmentAddress = forms.IntegerField(
-        label='Apartment Address',
-        required=False,
-        validators=[MinValueValidator(1)]
+    apartment_number = forms.IntegerField(
+        label="Apartment Address", required=False, validators=[MinValueValidator(1)]
     )
     street_address = forms.CharField(
-        label='Street Address',
-        max_length=90,
-        required=False
+        label="Street Address", max_length=90, required=False
     )
 
     class Meta:
         model = User
-        fields = ['name', 'email', 'surname', 'phone_number', 'gender']
+        fields = ["first_name", "email", "last_name", "phone_number", "gender"]
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
 
-        appartment_address = self.cleaned_data.get('appartmentAddress')
-        street_address = self.cleaned_data.get('street_address')
+        appartment_address = self.cleaned_data.get("apartment_number")
+        street_address = self.cleaned_data.get("street_address")
 
         if appartment_address or street_address:
             # Если введены данные для адреса
             address = Address.objects.create(
-                appartmentAddress=appartment_address,
-                street_address=street_address
+                apartment_number=appartment_address, street_address=street_address
             )
             user.address = address
 
         if commit:
             user.save()
 
-        return user    
-    
-    
+        return user
+
+
 class LoginForm(AuthenticationForm):
     class Meta:
         model = User
-        fields = ['email'] 
-    
-        
+        fields = ["email"]
+
+
 class EditForm(UserChangeForm):
     class Meta:
         model = User
-        fields = ['name', 'surname', 'phone_number', 'photo', 'gender']
+        fields = ["first_name", "last_name", "phone_number", "photo", "gender"]
 
     address_form = AddressForm()
 
@@ -100,10 +99,10 @@ class EditForm(UserChangeForm):
 
     def save(self, commit=True):
         user = super(EditForm, self).save(commit=False)
-        
+
         # Обработка данных формы адреса
         address_form = AddressForm(self.data, instance=self.instance.address)
-        
+
         if address_form.is_valid():
             address = address_form.save(commit=False)
             address.save()
@@ -112,52 +111,56 @@ class EditForm(UserChangeForm):
         if commit:
             user.save()
 
-        return user 
-    
-        
+        return user
+
+
 class DeleteForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['confirm_deletion']
+        fields = ["confirm_deletion"]
 
     confirm_deletion = forms.BooleanField(
         required=True,
-        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-        help_text='Подтвердите удаление пользователя'
+        widget=forms.CheckboxInput(attrs={"class": "form-check-input"}),
+        help_text="Подтвердите удаление пользователя",
     )
-    
- 
+
+
 class WishListItemForm(forms.ModelForm):
-    product = forms.ModelChoiceField(queryset=Product.objects.all(), 
-                                     label='Product',  to_field_name='name', required=True)    
+    product = forms.ModelChoiceField(
+        queryset=Product.objects.all(),
+        label="Product",
+        to_field_name="name",
+        required=True,
+    )
+
     class Meta:
         model = WishListItem
-        fields = ['product']   
-        
+        fields = ["product"]
+
 
 # class WishListForm(forms.ModelForm):
-#     wishList_item = forms.ModelMultipleChoiceField(queryset=WishListItem.objects.all(), 
+#     wishList_item = forms.ModelMultipleChoiceField(queryset=WishListItem.objects.all(),
 #                                                    widget=forms.Select, label='Любимые товары',
 #                                                    to_field_name='product')
 class WishListForm(forms.ModelForm):
     wishList_item = forms.ModelMultipleChoiceField(
-        queryset=WishListItem.objects.values_list('product__name', flat=True),
+        queryset=WishListItem.objects.values_list("product__name", flat=True),
         widget=forms.Select,
-        label='Любимые товары')
+        label="Любимые товары",
+    )
+
     class Meta:
         model = WishList
-        fields = ['wishList_item']    
-        
+        fields = ["wishList_item"]
+
     #     wishlist_item_form = WishListItemForm()
-        
-        
+
     # def __init__(self, *args, **kwargs):
-    #     super(WishListForm, self).__init__(*args, **kwargs)    
-        
+    #     super(WishListForm, self).__init__(*args, **kwargs)
+
     #     if self.instance.wishlist:
     #         self.wishlist_item_form = WishListItemForm(instance=self.instance.wishlist)
 
     # def save(self, commit=True):
     #     user = super(WishListForm, self).save(commit=False)
-        
-                          

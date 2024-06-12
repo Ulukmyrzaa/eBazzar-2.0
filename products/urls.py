d2374ab270fe50e6fb5b2b3070c876a01cf7e549
sinqx <sinqxpls@gmail.com>
2024-06-12 17:21:37 +0600

    change path for product categories

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
 from products import *
 
 urlpatterns = [
     path("create/", CreateProductView.as_view(), name="create-product"),
     path('', ProductListView.as_view(), name='product-list'),
-    path('<slug:category_slug>/', ProductListView.as_view(), name='product_list'),
-    path('<slug:category_slug>/<slug:product_slug>/', ProductDetailsView.as_view(), name='product_detail'),
+    re_path(r'^(?P<category_path>[-\w]+(?:/[-\w]+)*)', ProductListView.as_view(), name='products_list'),
+    #path('<slug:category_slug>/<slug:product_slug>/', ProductDetailsView.as_view(), name='product_detail'),
 ]
